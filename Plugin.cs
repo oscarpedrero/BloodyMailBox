7962c74b4b3b0da7c7d91d2a3325dfb0b8753ef8
oscarpedrero <oscarpedrero@gmail.com>
2023-07-29 22:52:05 +0200

    Agregar archivos de proyecto.

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using BepInEx.Configuration;
+using VampireCommandFramework;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace BloodyMailBox
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("gg.deca.VampireCommandFramework")]
+    public class Plugin : BasePlugin
+    {
+
+        public static ManualLogSource Logger;
+
+        private Harmony _harmony;
+
+        public override void Load()
+        {
+            // Plugin startup logic
+            CommandRegistry.RegisterAll();
+            _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            
+        }
+    }
+}
---
+++ Server/Commands/MailBoxCommands.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VampireCommandFramework;
+
+namespace BloodyMailBox.Server.Commands
+{
+    [CommandGroup("mailbox")]
+    internal class MailBoxCommands
+    {
+
+        [Command("send", usage: "\"<UserNick>\" \"<Message>\"", description: "Open a ticket to the server administrators", adminOnly: false)]
+        public static void SendMail(ChatCommandContext ctx, string name, string message)
+        {
+
+
+
+        }
+
+        [Command("open", usage: "\"<UserNick>\" \"<Message>\"", description: "Reply a Ticket", adminOnly: false)]
+        public static void OpenMail(ChatCommandContext ctx, string name, string message)
+        {
+
+        }
+
+        [Command("list", usage: "\"<UserNick>\" \"<Message>\"", description: "Reply a Ticket", adminOnly: false)]
+        public static void ListMail(ChatCommandContext ctx, string name, string message)
+        {
+
+        }
+    }
+}
